3116eed651c0aafdb5aee25f425395e2dfa5f269
joshuacassidy <Joshcassidy79@gmail.com>
2017-07-15 21:58:20 +0100

    Added code to clear console each time the program is run

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args){
+        //Clear console for unix terminals and emulators will not work with window cmd.exe
+        System.out.print("\033[H\033[2J");
         Toyota toyota = new Toyota((int) Math.random() * 5);
         toyota.accelerate((int) (Math.random() * 25));
         toyota.steer((int) (Math.random() * 100));
